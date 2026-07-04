710d4c44c71aae0830b48130d6b6dac0a57bac16
KritishAlli <113045963+KritishAlli@users.noreply.github.com>
2026-07-04 00:53:00 -0500

    Add: find-minimum-in-rotated-sorted-array - submission-7

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-7.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findMinRec(self, a1: List[int], a2: List[int]):
+        if (a1[0] < a2[-1]):
+            return a1[0]
+        if (len(a2) == 1):
+            if a1[0] < a2[0]:
+                return a1[0]
+            else:
+                return a2[0]
+        elif (len(a2) == 2 and len(a1) == 1):
+            if (a1[0] > a2[0]):
+                return a2[0]
+            elif (a1[0] > a2[1]):
+                return a2[1]
+            else:
+                return a1[0]
+        selected_arr = a2
+        
+        if (a1[0] > a1[-1]):
+            selected_arr = a1
+        arrlen = len(selected_arr)
+        arr1 = selected_arr[0:arrlen//2]
+        arr2 = selected_arr[arrlen//2:arrlen]
+
+        return self.findMinRec(arr1, arr2)
+
+    def findMin(self, nums: List[int]) -> int:
+        numlen = len(nums)
+        if (numlen == 1):
+            return nums[0]
+
+        arr1 = nums[0:numlen//2]
+        arr2 = nums[numlen//2:numlen]
+
+        return self.findMinRec(arr1, arr2)
